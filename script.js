d1a9ad14a0e94f41c5ce9b51ee7d7a5ab6ab1cc8
andrey <andreyten2006@gmail.com>
2021-08-30 11:59:50 +0500

    JavaScript-5

--- script.js
+++ script.js
@@ -1,47 +1,61 @@
-alert('Задание 1');
+// alert('Задание 1');
 
 
 
-const users = {
+// const users = {
 
-}
+// }
 
-for (let i = 1; i <= 3; i++) {
-    var username = prompt('Введите имя пользователя ' + i);
-    var age = prompt('Введите возраст пользователя ' + i);
+// for (let i = 1; i <= 10; i++) {
+//     var username = prompt('Введите имя пользователя ' + i);
+//     var age = prompt('Введите возраст пользователя ' + i);
 
-    const id = {
-        Имя: username,
-        Возраст: age
-    }
+//     const id = {
+//         Имя: username,
+//         Возраст: age
+//     }
 
-    console.log('Пользователь ' + i);
-    for(let key in id){
-        console.log(key + ' ' + id[key]);
-    }
+//     console.log('Пользователь ' + i);
+//     for(let key in id){
+//         console.log(key + ' ' + id[key]);
+//     }
 
-    users[id + i] = {
-        Имя: username,
-        Возраст: age
-    }
-}
+//     users[id + i] = {
+//         Имя: username,
+//         Возраст: age
+//     }
+// }
 
-console.log(users);
+// console.log(users);
 
 
 
 // alert('Задание 2')
 
 
-// console.log(receipt());
+console.log(receipt());
 
 // for(var key in receipt()){
 //     console.log(key);
-//     console.log(receipt()[key]) ;
+//     // console.log(receipt()[key]) ;
 //     for(const newKey in receipt()[key]){  
-//         console.log( receipt()[key][newKey]); 
-        
+//         console.log(newKey + ' ' +  receipt()[key][newKey]); 
 //     }
-//     var list = key + list
+//     var info = info + receipt()[key][info];
+//     var list = key + ' ' + list
 // }
 // console.log('Вы заказали: ' + list);
+
+
+// for(const key in receipt()){
+//     for(const newKey in receipt()[key]){
+//         var info = receipt()[key][newKey];
+//         var price = receipt()[key][newKey];
+//     }
+//     var list = list + ' ' + key + info +  ',';
+//     var mainprice = mainprice + price;
+// }
+
+// mainprice = mainprice + 9000;
+
+// console.log('Вы заказали: ' +  list + ' общая стоимость: ' + mainprice + '. С доставкой [9000]');
